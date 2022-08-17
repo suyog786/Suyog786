5ddced3dcd9b2010d3c873991b37eeb0b3b75094
suyog <suyog@gmail.com>
2022-08-17 15:35:04 +0530

    Commit with visual studio console app

---
+++ Visual Studio Console application.cs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 0; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine("The Even number is:  " + i);
+            }
+        }
+    }
+}
